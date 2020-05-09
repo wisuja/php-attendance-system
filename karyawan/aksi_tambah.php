b53f8270ddc304e94137dee304ceaff2f79d1e15
wisuja <williamsj26.wsj@gmail.com>
2020-05-09 11:53:04 +0700

    Fix time in Karyawan Absensi

--- karyawan/aksi_tambah.php
+++ karyawan/aksi_tambah.php
@@ -7,6 +7,7 @@ $nama_karyawan = $_SESSION["username"];
 $lokasi = $_POST["lokasi"];
 $gambar = $_FILES["gambar"]["name"];
 $pesan = $_POST["pesan"];
+$waktu = date('Y-m-d H:i:s');
 
 $ekstensi_diperbolehkan = array('png', 'jpg', 'jpeg');
 $x = explode('.', $gambar);
@@ -17,7 +18,7 @@ $nama_gambar_baru = $angka_acak . '-' . $gambar;
 
 if (in_array($ekstensi, $ekstensi_diperbolehkan) === true) {
   move_uploaded_file($file_tmp, '../img/uploaded_img/' . $nama_gambar_baru);
-  $query = "INSERT INTO absensi (nama_karyawan, waktu, lokasi, pesan, gambar) VALUES ('$nama_karyawan', NOW(), '$lokasi', '$pesan', '$nama_gambar_baru')";
+  $query = "INSERT INTO absensi (nama_karyawan, waktu, lokasi, pesan, gambar) VALUES ('$nama_karyawan', '$waktu', '$lokasi', '$pesan', '$nama_gambar_baru')";
   $result = mysqli_query($koneksi, $query);
   if (!$result) {
     die("Query gagal dijalankan: " . mysqli_errno($koneksi) . " - " . mysqli_error($koneksi));
